3ef67da86bafe02aff7cef4a2c461de58a6187be
Kostyantyn Leschenko <k.s.leschenko@gmail.com>
2012-05-26 16:37:30 +0300

    ticket:6 added calendar widget and ajax responses to UpdateView

--- apps/contacts/views.py
+++ apps/contacts/views.py
@@ -1,8 +1,10 @@
+import json
+import time
 from django.views.generic import TemplateView, UpdateView
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.utils.decorators import method_decorator
-from django.http import Http404
+from django.http import Http404, HttpResponse, HttpResponseBadRequest
 from contacts.models import Person
 from contacts.forms import ContactsEditForm
 
@@ -32,6 +34,21 @@ class PersonUpdateView(UpdateView):
         except ObjectDoesNotExist:
             raise Http404(u"No contacts records found in the database")
         return obj
+
+    def form_valid(self, form):
+        if self.request.is_ajax():
+            self.object = form.save()
+            time.sleep(5)
+            return HttpResponse(json.dumps('success'),
+                    mimetype='application/json')
+        return super(PersonUpdateView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        if self.request.is_ajax():
+            time.sleep(5)
+            return HttpResponseBadRequest(json.dumps(form.errors),
+                    mimetype='application/json')
+        return super(PersonUpdateView, self).form_invalid(form)
 
     @method_decorator(login_required)
     @method_decorator(user_passes_test(lambda u: u.is_staff))
