429e0e9327ab2c9f267ae03ba2adb5b9a3248743
Kostyantyn Leschenko <k.s.leschenko@gmail.com>
2012-05-29 17:19:21 +0300

    ticket:9 fixed script

--- run_command.sh
+++ run_command.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-VENV=../
-source $VENV/bin/activate && PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=django_test.settings python manage.py list_models 2> `date +'%Y.%m.%d'`.dat
+PROJECT_ROOT=`pwd`
+VENV_ROOT=`dirname $PROJECT_ROOT`
+
+source $VENV_ROOT/bin/activate && python $PROJECT_ROOT/manage.py list_models 2> `date +'%Y.%m.%d'`.dat
