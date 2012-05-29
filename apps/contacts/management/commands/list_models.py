825844cac1ccc7fc260732663917f97940a3619b
Kostyantyn Leschenko <k.s.leschenko@gmail.com>
2012-05-29 18:00:05 +0300

    ticket:9 removed venv activation from bash script

--- apps/contacts/management/commands/list_models.py
+++ apps/contacts/management/commands/list_models.py
@@ -9,7 +9,7 @@ class Command(NoArgsCommand):
         app_models = models.get_models()
         for model in app_models:
             ct = ContentType.objects.get_for_model(model)
-            output = 'Model: %20s,\tObjects: %s\n' % (ct.app_label + '.' + ct.model,
+            output = 'Model: %s.%s,\tObjects: %s\n' % (ct.app_label, ct.model,
                 model.objects.count())
             self.stdout.write(output)
             self.stderr.write('error: ' + output)
--- run_command.sh
+++ run_command.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT=`pwd`
-VENV_ROOT=`dirname $PROJECT_ROOT`
-
-source $VENV_ROOT/bin/activate && python $PROJECT_ROOT/manage.py list_models 2> `date +'%Y.%m.%d'`.dat
+python manage.py list_models 2> `date +'%Y.%m.%d'`.dat
